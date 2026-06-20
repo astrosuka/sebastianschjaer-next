aaf55eb84fdd42801179798bb6a0a3a7c0f2800f
astrosuka <yegresvoselok@gmail.com>
2026-06-20 12:27:30 -0300

    Fix webhook secret verification

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,14 +1,16 @@
 import { revalidatePath } from "next/cache"
 import { NextRequest, NextResponse } from "next/server"
+import { isValidSignature, SIGNATURE_HEADER_NAME } from "@sanity/webhook"
 
 export async function POST(request: NextRequest) {
-  const signature = request.headers.get("sanity-webhook-signature")
-  if (signature !== process.env.SANITY_WEBHOOK_SECRET) {
+  const body = await request.text()
+  const signature = request.headers.get(SIGNATURE_HEADER_NAME) ?? ""
+
+  if (!isValidSignature(body, signature, process.env.SANITY_WEBHOOK_SECRET!)) {
     return NextResponse.json({ message: "Invalid secret" }, { status: 401 })
   }
 
-  const body = await request.json()
-  const { _type, slug } = body
+  const { _type, slug } = JSON.parse(body)
 
   switch (_type) {
     case "director":
